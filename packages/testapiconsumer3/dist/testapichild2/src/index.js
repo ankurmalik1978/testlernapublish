0e584ef426922a3e285af62e835bb49172756a26
Ankur Malik <ankur.malik@sap.com>
2020-01-27 22:10:13 +0530

    deleted files

--- packages/testapiconsumer3/dist/testapichild2/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Students_1 = require("./Students");
-exports.Students = Students_1.Students;
-//# sourceMappingURL=index.js.map
